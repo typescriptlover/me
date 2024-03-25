828007b5d013713ed73c676cc5bb535a4b34a20e
typescriptlover <vigywadhwani@gmail.com>
2024-03-25 18:22:46 -0400

    close the gap between works

--- src/components/Home/Works/Works.tsx
+++ src/components/Home/Works/Works.tsx
@@ -5,7 +5,7 @@ const Works = () => {
    return (
       <div className="mt-32 sm:mt-36 lg:mt-44 grid grid-cols-1 lg:grid-cols-3">
          <div className="hidden lg:block lg:col-span-1"></div>
-         <div className="col-span-1 lg:col-span-2 flex flex-col gap-y-16 sm:gap-y-24 lg:gap-y-28">
+         <div className="col-span-1 lg:col-span-2 flex flex-col gap-y-14">
             {works.map((work, workIndex) => (
                <Work key={workIndex} index={workIndex} {...work} />
             ))}
