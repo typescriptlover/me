591e641ede5e1abecb52877dd67f62741e81d73a
typescriptlover <vigywadhwani@gmail.com>
2025-03-02 23:03:46 -0400

    fix images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
    /* config options here */
    output: 'export',
+   images: {
+      loader: 'custom',
+      loaderFile: './src/lib/loader.js',
+   },
 };
 
 export default nextConfig;
